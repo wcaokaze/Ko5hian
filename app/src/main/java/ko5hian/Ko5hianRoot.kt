95ec4996608bc2b3fdcc2f44762485b31a03ea76
wcaokaze <wcaokaze@gmail.com>
2018-10-31 06:09:11 +0900

    added ko5hian function

--- app/src/main/java/ko5hian/Ko5hianRoot.kt
+++ app/src/main/java/ko5hian/Ko5hianRoot.kt
@@ -2,4 +2,4 @@ package ko5hian
 
 import android.content.Context
 
-class Ko5hianRoot(val context: Context)
+inline class Ko5hianRoot(val context: Context)
--- app/src/main/java/ko5hian/runtime.kt
+++ app/src/main/java/ko5hian/runtime.kt
@@ -1,8 +1,14 @@
 package ko5hian
 
+import android.content.Context
+import android.view.View
+
 @DslMarker
 @Target(AnnotationTarget.TYPE)
 annotation class Ko5hianViewDslMarker
+
+inline fun <V : View> ko5hian(context: Context, builder: Ko5hianRoot.() -> V): V
+        = Ko5hianRoot(context).builder()
 
 const val MATCH_PARENT = android.view.ViewGroup.LayoutParams.MATCH_PARENT
 const val WRAP_CONTENT = android.view.ViewGroup.LayoutParams.WRAP_CONTENT
