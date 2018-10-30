f48a48b02ffab1a18c872a034f76e5aea9050e11
wcaokaze <wcaokaze@gmail.com>
2018-10-31 02:01:51 +0900

    added contract

--- app/src/main/java/ko5hian/TextView.kt
+++ app/src/main/java/ko5hian/TextView.kt
@@ -1,5 +1,7 @@
 package ko5hian
 
+import kotlin.contracts.*
+
 typealias TextViewOn<P> = android.widget.TextView
 
 val TextViewOn<android.widget.FrameLayout>.parentView: android.widget.FrameLayout get() = parent as android.widget.FrameLayout
@@ -12,14 +14,24 @@ fun TextViewOn<android.widget.FrameLayout>.lParams(operation: (@Ko5hian android.
     return lParams
 }
 
+@ExperimentalContracts
 inline fun Ko5hianRoot.`textView`(initializer: (@Ko5hian android.widget.TextView).() -> Unit): android.widget.TextView {
+    contract {
+        callsInPlace(initializer, InvocationKind.EXACTLY_ONCE)
+    }
+
     val v = android.widget.TextView(context)
     v.layoutParams = android.view.ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
     v.initializer()
     return v
 }
 
+@ExperimentalContracts
 inline fun android.widget.FrameLayout.`textView`(initializer: (@Ko5hian TextViewOn<android.widget.FrameLayout>).() -> Unit): TextViewOn<android.widget.FrameLayout> {
+    contract {
+        callsInPlace(initializer, InvocationKind.EXACTLY_ONCE)
+    }
+
     val v = android.widget.TextView(context)
     v.layoutParams = android.widget.FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
     v.initializer()
