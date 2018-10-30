018465e89486ec9b67f89e47206926ad04f3b6d7
wcaokaze <wcaokaze@gmail.com>
2018-10-31 06:11:08 +0900

    added a contract to ko5hian

--- app/src/main/java/ko5hian/runtime.kt
+++ app/src/main/java/ko5hian/runtime.kt
@@ -3,12 +3,20 @@ package ko5hian
 import android.content.Context
 import android.view.View
 
+import kotlin.contracts.*
+
 @DslMarker
 @Target(AnnotationTarget.TYPE)
 annotation class Ko5hianViewDslMarker
 
-inline fun <V : View> ko5hian(context: Context, builder: Ko5hianRoot.() -> V): V
-        = Ko5hianRoot(context).builder()
+@ExperimentalContracts
+inline fun <V : View> ko5hian(context: Context, builder: Ko5hianRoot.() -> V): V {
+    contract {
+        callsInPlace(builder, InvocationKind.EXACTLY_ONCE)
+    }
+
+    return Ko5hianRoot(context).builder()
+}
 
 const val MATCH_PARENT = android.view.ViewGroup.LayoutParams.MATCH_PARENT
 const val WRAP_CONTENT = android.view.ViewGroup.LayoutParams.WRAP_CONTENT
