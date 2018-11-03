07b4756b39757753839c143b128eb341707f53f5
wcaokaze <wcaokaze@gmail.com>
2018-11-04 03:03:00 +0900

    reconsidered DSL

--- app/src/main/java/ko5hian/TextView.kt
+++ app/src/main/java/ko5hian/TextView.kt
@@ -1,44 +1,49 @@
 package ko5hian
 
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlin.contracts.*
 
-typealias TextViewOn<P> = android.widget.TextView
-
-inline val TextViewOn<android.widget.FrameLayout>.parentView: android.widget.FrameLayout get() = parent as android.widget.FrameLayout
-
-inline val TextViewOn<android.widget.FrameLayout>.lParams: android.widget.FrameLayout.LayoutParams get() = layoutParams as android.widget.FrameLayout.LayoutParams
-
 @ExperimentalContracts
-inline fun TextViewOn<android.widget.FrameLayout>.lParams(operation: (@Ko5hianViewDslMarker android.widget.FrameLayout.LayoutParams).() -> Unit): android.widget.FrameLayout.LayoutParams {
+inline fun Ko5hianRoot.`textView`(builder: Ko5hianViewHolder<TextView, ViewGroup.LayoutParams>.() -> Unit): TextView {
     contract {
-        callsInPlace(operation, InvocationKind.EXACTLY_ONCE)
+        callsInPlace(builder, InvocationKind.EXACTLY_ONCE)
     }
 
-    val lParams = lParams
-    layoutParams = lParams.apply(operation)
-    return lParams
-}
-
-@ExperimentalContracts
-inline fun Ko5hianRoot.`textView`(initializer: (@Ko5hianViewDslMarker android.widget.TextView).() -> Unit): android.widget.TextView {
-    contract {
-        callsInPlace(initializer, InvocationKind.EXACTLY_ONCE)
-    }
-
-    val v = android.widget.TextView(context)
-    v.layoutParams = android.view.ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-    v.initializer()
+    val v = TextView(context)
+    val l = ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+    v.layoutParams = l
+    val vh = Ko5hianViewHolder(context, v, l)
+    vh.builder()
     return v
 }
 
-@ExperimentalContracts
-inline fun android.widget.FrameLayout.`textView`(initializer: (@Ko5hianViewDslMarker TextViewOn<android.widget.FrameLayout>).() -> Unit): TextViewOn<android.widget.FrameLayout> {
+@ExperimentalContracts @JvmName("textViewForFrameLayout")
+inline fun Ko5hianViewHolder<FrameLayout, *>.`textView`(builder: Ko5hianViewHolder<TextView, FrameLayout.LayoutParams>.() -> Unit): TextView {
     contract {
-        callsInPlace(initializer, InvocationKind.EXACTLY_ONCE)
+        callsInPlace(builder, InvocationKind.EXACTLY_ONCE)
     }
 
-    val v = android.widget.TextView(context)
-    v.layoutParams = android.widget.FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-    v.initializer()
+    val v = TextView(context)
+    val l = FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+    v.layoutParams = l
+    val vh = Ko5hianViewHolder(context, v, l)
+    vh.builder()
+    return v
+}
+
+@ExperimentalContracts @JvmName("textViewForLinearLayout")
+inline fun Ko5hianViewHolder<LinearLayout, *>.`textView`(builder: Ko5hianViewHolder<TextView, LinearLayout.LayoutParams>.() -> Unit): TextView {
+    contract {
+        callsInPlace(builder, InvocationKind.EXACTLY_ONCE)
+    }
+
+    val v = TextView(context)
+    val l = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+    v.layoutParams = l
+    val vh = Ko5hianViewHolder(context, v, l)
+    vh.builder()
     return v
 }
--- app/src/main/java/ko5hian/runtime.kt
+++ app/src/main/java/ko5hian/runtime.kt
@@ -2,12 +2,22 @@ package ko5hian
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 
 import kotlin.contracts.*
 
 @DslMarker
+@Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.TYPE)
 annotation class Ko5hianViewDslMarker
+
+class Ko5hianViewHolder<V : View, L : ViewGroup.LayoutParams>(
+    val context: Context,
+    val view: V,
+    val layout: L
+)
+
+class Ko5hianRoot(val context: Context)
 
 @ExperimentalContracts
 inline fun <V : View> ko5hian(context: Context, builder: Ko5hianRoot.() -> V): V {
@@ -16,6 +26,12 @@ inline fun <V : View> ko5hian(context: Context, builder: Ko5hianRoot.() -> V): V
     }
 
     return Ko5hianRoot(context).builder()
+}
+
+inline fun <V : View> ko5hian(view: V, builder: Ko5hianViewHolder<V, *>.() -> V): V {
+    val vh = Ko5hianViewHolder(view.context, view, view.layoutParams)
+    vh.builder()
+    return view
 }
 
 const val MATCH_PARENT = android.view.ViewGroup.LayoutParams.MATCH_PARENT
