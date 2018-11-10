2c7db6e88125f2cc3270e06f120bf2b87a42a0b4
wcaokaze <wcaokaze@gmail.com>
2018-11-10 23:02:44 +0900

    added to add src dir

--- ko5hian/src/main/groovy/com/wcaokaze/ko5hian/Ko5hian.groovy
+++ ko5hian/src/main/groovy/com/wcaokaze/ko5hian/Ko5hian.groovy
@@ -4,19 +4,21 @@ import org.gradle.api.*
 
 class Ko5hian implements Plugin<Project> {
     void apply(Project project) {
+        def outDir = project.file("$project.buildDir/generated/source/ko5hian/src/")
         def configurations = project.container(Ko5hianConfiguration)
 
         configurations.all {
             def parsedConfiguration = ParsedConfigurationKt.parseConfiguration(it)
 
-            def generator = new Ko5hianGenerator(
-                    project.file("$project.buildDir/generated/source/ko5hian/src/"),
-                    parsedConfiguration)
+            def generator = new Ko5hianGenerator(outDir, parsedConfiguration)
 
             generator.writeRuntime()
             generator.writeKo5hian()
         }
 
         project.extensions.ko5hian = configurations
+
+        project.android.sourceSets.main.java  .srcDirs += outDir
+        project.android.sourceSets.main.kotlin.srcDirs += outDir
     }
 }
