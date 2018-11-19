c6982801f452cf9ed529717956d1c44fdeec92e5
wcaokaze <wcaokaze@gmail.com>
2018-11-20 03:01:32 +0900

    avoid to save hash file into src

--- src/main/groovy/com/wcaokaze/ko5hian/Ko5hian.groovy
+++ src/main/groovy/com/wcaokaze/ko5hian/Ko5hian.groovy
@@ -3,14 +3,18 @@ package com.wcaokaze.ko5hian
 import org.gradle.api.*
 
 class Ko5hian implements Plugin<Project> {
+    private File ko5hianRootDir
+    private File ko5hianSrcDir
+
     void apply(Project project) {
-        def outDir = project.file("$project.buildDir/generated/source/ko5hian/src/")
+        ko5hianRootDir = project.file("$project.buildDir/generated/source/ko5hian/")
+        ko5hianSrcDir = new File(ko5hianRootDir, 'src/')
 
         project.metaClass.getKo5hian = {
             new Ko5hianConfigurator() {
                 def outputConfig(Closure configAction) {
                     super.outputConfig(configAction)
-                    generateInto(outDir, configurations)
+                    generateInto(configurations)
                 }
             }
         }
@@ -21,34 +25,34 @@ class Ko5hian implements Plugin<Project> {
             configAction.delegate = configurator
             configAction()
 
-            generateInto(outDir, configurator.configurations)
+            generateInto(configurator.configurations)
         }
 
-        project.android.sourceSets.main.java  .srcDirs += outDir
-        project.android.sourceSets.main.kotlin.srcDirs += outDir
+        project.android.sourceSets.main.java  .srcDirs += ko5hianSrcDir
+        project.android.sourceSets.main.kotlin.srcDirs += ko5hianSrcDir
     }
 
-    private def generateInto(File outDir, List<Ko5hianConfiguration> configurations) {
+    private def generateInto(List<Ko5hianConfiguration> configurations) {
         def parsedConfigs = configurations
                 .collect { ParsedConfigurationKt.parseConfiguration(it) }
 
         def hash = parsedConfigs.collect { it.ingredientsHash } .join()
-        def hashFile = new File(outDir, 'hash')
+        def hashFile = new File(ko5hianRootDir, 'hash')
 
         try {
             if (hashFile.text == hash) return
         } catch (IOException ignored) {
         }
 
-        outDir.deleteDir()
-        outDir.mkdirs()
+        ko5hianSrcDir.deleteDir()
+        ko5hianSrcDir.mkdirs()
 
         hashFile.text = hash
 
         for (def config : configurations) {
             def parsedConfig = ParsedConfigurationKt.parseConfiguration(config)
 
-            def generator = new Ko5hianGenerator(outDir, parsedConfig)
+            def generator = new Ko5hianGenerator(ko5hianSrcDir, parsedConfig)
 
             generator.writeRuntime()
             generator.writeKo5hian()
