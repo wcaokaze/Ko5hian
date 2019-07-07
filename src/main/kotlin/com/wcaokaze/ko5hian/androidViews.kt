824b6f1b220651525e0e98b549c40deb2f3144a7
wcaokaze <wcaokaze@gmail.com>
2019-07-07 21:11:50 +0900

    added WebView into androidViews

--- src/main/kotlin/com/wcaokaze/ko5hian/androidViews.kt
+++ src/main/kotlin/com/wcaokaze/ko5hian/androidViews.kt
@@ -1,5 +1,6 @@
 package com.wcaokaze.ko5hian
 
 val androidViews = listOf(
-      ViewConfiguration("android.view.View")
+      ViewConfiguration("android.view.View"),
+      ViewConfiguration("android.webkit.WebView")
 )
