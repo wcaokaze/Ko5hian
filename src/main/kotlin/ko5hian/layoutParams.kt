60c1c653e7b8b19dc9dea7fd8b35799cdfaea9af
wcaokaze <wcaokaze@gmail.com>
2019-09-19 03:01:45 +0900

    deleted files for development Gradle plugin

--- src/main/kotlin/ko5hian/layoutParams.kt
+++ src/main/kotlin/ko5hian/layoutParams.kt
@@ -1,11 +1,30 @@
 @file:Suppress("UNUSED")
 package ko5hian
 
+import android.content.Context
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
 
 val MATCH_PARENT = ViewGroup.LayoutParams.MATCH_PARENT
 val WRAP_CONTENT = ViewGroup.LayoutParams.WRAP_CONTENT
+
+fun Context.dip(dip: Float): Int {
+   val px = (dip * resources.displayMetrics.density).toInt()
+
+   return when {
+      px != 0 -> px
+      dip < 0 -> -1
+      else    ->  1
+   }
+}
+
+@Suppress("NOTHING_TO_INLINE") inline fun Context.dip(dip: Int)    = dip(dip.toFloat())
+@Suppress("NOTHING_TO_INLINE") inline fun Context.dip(dip: Double) = dip(dip.toFloat())
+
+@Suppress("NOTHING_TO_INLINE") inline fun View.dip(dip: Int)    = context.dip(dip)
+@Suppress("NOTHING_TO_INLINE") inline fun View.dip(dip: Float)  = context.dip(dip)
+@Suppress("NOTHING_TO_INLINE") inline fun View.dip(dip: Double) = context.dip(dip)
 
 val Ko5hianBuilder<*, *>.START  get() = Gravity.START
 val Ko5hianBuilder<*, *>.TOP    get() = Gravity.TOP
