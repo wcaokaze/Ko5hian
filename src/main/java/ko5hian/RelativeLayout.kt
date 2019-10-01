c5933106ca622435085ad57103705f6e53bc7ee2
wcaokaze <wcaokaze@gmail.com>
2019-10-02 02:43:37 +0900

    added RelativeLayoutParams extensions for API Level 17

--- src/main/java/ko5hian/RelativeLayout.kt
+++ src/main/java/ko5hian/RelativeLayout.kt
@@ -4,6 +4,7 @@ package ko5hian
 import android.view.ViewGroup
 import android.view.ViewManager
 import android.widget.RelativeLayout
+import androidx.annotation.RequiresApi
 
 import kotlin.contracts.*
 
@@ -37,6 +38,13 @@ inline fun <P : ViewGroup, L> Ko5hian<P, *, L>.relativeLayout(
          ko5hianAction
    )
 }
+
+val Ko5hianLayoutParams<RelativeLayout.LayoutParams>.START_OF            @RequiresApi(17) inline get() = RelativeLayout.START_OF
+val Ko5hianLayoutParams<RelativeLayout.LayoutParams>.END_OF              @RequiresApi(17) inline get() = RelativeLayout.END_OF
+val Ko5hianLayoutParams<RelativeLayout.LayoutParams>.ALIGN_START         @RequiresApi(17) inline get() = RelativeLayout.ALIGN_START
+val Ko5hianLayoutParams<RelativeLayout.LayoutParams>.ALIGN_END           @RequiresApi(17) inline get() = RelativeLayout.ALIGN_END
+val Ko5hianLayoutParams<RelativeLayout.LayoutParams>.ALIGN_PARENT_START  @RequiresApi(17) inline get() = RelativeLayout.ALIGN_PARENT_START
+val Ko5hianLayoutParams<RelativeLayout.LayoutParams>.ALIGN_PARENT_END    @RequiresApi(17) inline get() = RelativeLayout.ALIGN_PARENT_END
 
 val Ko5hianLayoutParams<RelativeLayout.LayoutParams>.TRUE                inline get() = RelativeLayout.TRUE
 val Ko5hianLayoutParams<RelativeLayout.LayoutParams>.ABOVE               inline get() = RelativeLayout.ABOVE
